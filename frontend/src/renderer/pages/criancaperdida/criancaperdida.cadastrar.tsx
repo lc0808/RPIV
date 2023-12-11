eb7193dc3798dbfe387a7fdd92be119723ad9119
Lucas Vieira <lucasvdc.aluno@unipampa.edu.br>
2023-12-11 00:03:03 -0300

    Add Cadastrar criancaPerdida

---
+++ frontend/src/renderer/pages/criancaperdida/criancaperdida.cadastrar.tsx
@@ -0,0 +1,141 @@
+import Swal from 'sweetalert2';
+import TopBar from '../../components/TopBar';
+import { useNavigate } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../../api/api';
+
+type TipoCriancaPerdida = {
+  nome: string;
+  descricao: string;
+  divisaoId: string;
+};
+
+export default function CadastrarSecao() {
+  const navegar = useNavigate();
+
+  const [criancaPerdida, setcriancaPerdida] = useState<TipoCriancaPerdida>({
+    nome: '',
+    descricao: '',
+    divisaoId: '',
+  });
+
+  const handleChange = (fieldName: keyof TipoCriancaPerdida, value: string) => {
+    setCriancaPerdida((criancaPerdida) => ({
+      ...prevCriancaPerdida,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleCancelar = () => {
+    Swal.fire({
+      title: 'Você tem certeza?',
+      text: 'Todos os dados preenchidos serão descartados.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar',
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Cadastro cancelado',
+          'Todos os dados preenchidos foram descartados.',
+          'success',
+        );
+        navegar('/criancaperdida');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+      }
+    });
+  };
+
+  async function handleCadastro(e: FormEvent) {
+    e.preventDefault();
+
+    await api
+      .post('/criancaperdida/nova', criancaPerdida)
+      .then((data: any) => {
+        navegar('/criancaperdida');
+        toast.success('Criança perdida cadastrada com sucesso!');
+      })
+      .catch((err: any) => {
+        toast.error('Criança perdida já existente ou campos inválidos!');
+      });
+  }
+
+  return (
+    <div>
+      <TopBar />
+
+      <form onSubmit={(e) => handleCadastro(e)}>
+        <div className="box">
+          <div className="create-area">
+            <div className="create-area-content">
+              <h1 className="title">Cadastrar Seção</h1>
+              <div id="msg"></div>
+              <div className="input-container">
+                <input
+                  type="text"
+                  className="input-text"
+                  name="nome"
+                  value={crianca.nome}
+                  onChange={(e) => handleChange('nome', e.target.value)}
+                  required
+                />
+                <label htmlFor="name" className="input-label">
+                  Nome Criança
+                </label>
+              </div>
+              <div className="input-container">
+                <input
+                  type="text"
+                  className="input-text"
+                  name="descricao"
+                  value={crianca.descricao}
+                  onChange={(e) => handleChange('descricao', e.target.value)}
+                  required
+                />
+                <label htmlFor="name" className="input-label">
+                  Descrição
+                </label>
+              </div>
+              <div className="input-container">
+                <input
+                  type="text"
+                  className="input-text"
+                  name="divisaoId"
+                  value={crianca.local}
+                  onChange={(e) => handleChange('divisaoId', e.target.value)}
+                  required
+                />
+                <label htmlFor="name" className="input-label">
+                  Local
+                </label>
+              </div>
+
+              <div className="btns">
+                <button
+                  type="button"
+                  id="cancel-btn"
+                  className="cancel-btn"
+                  onClick={handleCancelar}
+                  defaultChecked
+                >
+                  Cancelar
+                </button>
+
+                <input
+                  type="submit"
+                  className="input-submit"
+                  id="submit"
+                  value="Cadastrar"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="background-area"></div>
+        </div>
+      </form>
+    </div>
+  );
+}
